82763f30690d691b769bac2e19ce1000aac5a4dd
Benjamin Lind <benjamin.lind@northvolt.com>
2025-04-03 10:15:08 +0200

    Reveal quest text when > 4 people joins

--- Phoenix/myapp/lib/myapp_web/channels/room_channel.ex
+++ Phoenix/myapp/lib/myapp_web/channels/room_channel.ex
@@ -7,12 +7,18 @@ defmodule MyAppWeb.RoomChannel do
 
   def handle_in("message:new", %{"body" => body}, socket) do
     Counter.add(body)
-    # broadcast!(socket, "message:new", %{body: body})
-    IO.puts("About to call Counter.get_all()")
     all_names = Counter.get_all()
-    IO.puts("Did call Counter.get_all()")
+    cond do
+      length(all_names) > 4 ->
+        IO.puts("Sending start quest message to everyone")
+        broadcast!(socket, "message:new", %{body: "start_quest"})
+      true ->
+        IO.puts("Waiting for more people to join")
+    end
+
     IO.puts("All names: #{inspect(all_names)}")
     broadcast!(socket, "message:new", %{body: all_names})
+
     {:noreply, socket}
   end
 end
