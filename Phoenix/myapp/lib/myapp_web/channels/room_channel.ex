9c8b011dcb4fdb0d46410bc0ecbed491fe3922a4
Benjamin Lind <benjamin.lind@northvolt.com>
2025-04-03 22:52:48 +0200

    send coords & rename new -> new_joiner

--- Phoenix/myapp/lib/myapp_web/channels/room_channel.ex
+++ Phoenix/myapp/lib/myapp_web/channels/room_channel.ex
@@ -5,19 +5,26 @@ defmodule MyAppWeb.RoomChannel do
     {:ok, socket}
   end
 
-  def handle_in("message:new", %{"body" => body}, socket) do
+  def handle_in("message:new_joiner", %{"body" => body}, socket) do
     Counter.add(body)
     all_names = Counter.get_all()
     cond do
       length(all_names) > 4 ->
         IO.puts("Sending start quest message to everyone")
-        broadcast!(socket, "message:new", %{body: "start_quest"})
+        broadcast!(socket, "message:new_joiner", %{body: "start_quest"})
       true ->
         IO.puts("Waiting for more people to join")
     end
 
     IO.puts("All names: #{inspect(all_names)}")
-    broadcast!(socket, "message:new", %{body: all_names})
+    broadcast!(socket, "message:new_joiner", %{body: all_names})
+
+    {:noreply, socket}
+  end
+
+  def handle_in("message:coords", %{"lat" => lat, "long" => long}, socket) do
+
+    IO.puts("received #{lat}, #{long}")
 
     {:noreply, socket}
   end
--- Phoenix/myapp/lib/myapp_web/controllers/page_controller.ex
+++ Phoenix/myapp/lib/myapp_web/controllers/page_controller.ex
@@ -6,10 +6,9 @@ defmodule MyappWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
-  
+
   def hello(conn, _params) do
     count = MyApp.Counter.increment()
     text(conn, "whatsuppp #{count} >D")
   end
 end
-
